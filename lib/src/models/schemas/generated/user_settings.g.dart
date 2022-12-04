25e27765700ef705a215713ce37cd2a7535a19e6
Paul Nettleton <k9@k9withabone.dev>
2022-12-04 17:01:57 -0600

    Add user settings schema

--- lib/src/models/schemas/generated/user_settings.g.dart
+++ lib/src/models/schemas/generated/user_settings.g.dart
@@ -7,7 +7,27 @@ part of '../user_settings.dart';
 // **************************************************************************
 
 _$_UserSettings _$$_UserSettingsFromJson(Map<String, dynamic> json) =>
-    _$_UserSettings();
+    _$_UserSettings(
+      mobileOrderBy: json['mobileOrderBy'] as String,
+      mobileOrderDesc: json['mobileOrderDesc'] as bool,
+      mobileFilterBy: json['mobileFilterBy'] as String,
+      orderBy: json['orderBy'] as String,
+      orderDesc: json['orderDesc'] as bool,
+      filterBy: json['filterBy'] as String,
+      playbackRate: (json['playbackRate'] as num).toDouble(),
+      bookshelfCoverSize: json['bookshelfCoverSize'] as int,
+      collapseSeries: json['collapseSeries'] as bool,
+    );
 
 Map<String, dynamic> _$$_UserSettingsToJson(_$_UserSettings instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'mobileOrderBy': instance.mobileOrderBy,
+      'mobileOrderDesc': instance.mobileOrderDesc,
+      'mobileFilterBy': instance.mobileFilterBy,
+      'orderBy': instance.orderBy,
+      'orderDesc': instance.orderDesc,
+      'filterBy': instance.filterBy,
+      'playbackRate': instance.playbackRate,
+      'bookshelfCoverSize': instance.bookshelfCoverSize,
+      'collapseSeries': instance.collapseSeries,
+    };
