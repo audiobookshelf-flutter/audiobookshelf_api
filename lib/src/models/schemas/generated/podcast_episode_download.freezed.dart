b010b400db636b97b748982cc04e569480f74cf8
Paul Nettleton <k9@k9withabone.dev>
2022-12-04 09:16:35 -0600

    Add podcast episode download schema

---
+++ lib/src/models/schemas/generated/podcast_episode_download.freezed.dart
@@ -0,0 +1,358 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of '../podcast_episode_download.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+PodcastEpisodeDownload _$PodcastEpisodeDownloadFromJson(
+    Map<String, dynamic> json) {
+  return _PodcastEpisodeDownload.fromJson(json);
+}
+
+/// @nodoc
+mixin _$PodcastEpisodeDownload {
+  String get id => throw _privateConstructorUsedError;
+  String get episodeDisplayTitle => throw _privateConstructorUsedError;
+  Uri get url => throw _privateConstructorUsedError;
+  String get libraryItemId => throw _privateConstructorUsedError;
+  bool get isDownloading => throw _privateConstructorUsedError;
+  bool get isFinished => throw _privateConstructorUsedError;
+  bool get failed => throw _privateConstructorUsedError;
+  DateTime? get startedAt => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  DateTime? get finishedAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $PodcastEpisodeDownloadCopyWith<PodcastEpisodeDownload> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PodcastEpisodeDownloadCopyWith<$Res> {
+  factory $PodcastEpisodeDownloadCopyWith(PodcastEpisodeDownload value,
+          $Res Function(PodcastEpisodeDownload) then) =
+      _$PodcastEpisodeDownloadCopyWithImpl<$Res, PodcastEpisodeDownload>;
+  @useResult
+  $Res call(
+      {String id,
+      String episodeDisplayTitle,
+      Uri url,
+      String libraryItemId,
+      bool isDownloading,
+      bool isFinished,
+      bool failed,
+      DateTime? startedAt,
+      DateTime createdAt,
+      DateTime? finishedAt});
+}
+
+/// @nodoc
+class _$PodcastEpisodeDownloadCopyWithImpl<$Res,
+        $Val extends PodcastEpisodeDownload>
+    implements $PodcastEpisodeDownloadCopyWith<$Res> {
+  _$PodcastEpisodeDownloadCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? episodeDisplayTitle = null,
+    Object? url = null,
+    Object? libraryItemId = null,
+    Object? isDownloading = null,
+    Object? isFinished = null,
+    Object? failed = null,
+    Object? startedAt = freezed,
+    Object? createdAt = null,
+    Object? finishedAt = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      episodeDisplayTitle: null == episodeDisplayTitle
+          ? _value.episodeDisplayTitle
+          : episodeDisplayTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as Uri,
+      libraryItemId: null == libraryItemId
+          ? _value.libraryItemId
+          : libraryItemId // ignore: cast_nullable_to_non_nullable
+              as String,
+      isDownloading: null == isDownloading
+          ? _value.isDownloading
+          : isDownloading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isFinished: null == isFinished
+          ? _value.isFinished
+          : isFinished // ignore: cast_nullable_to_non_nullable
+              as bool,
+      failed: null == failed
+          ? _value.failed
+          : failed // ignore: cast_nullable_to_non_nullable
+              as bool,
+      startedAt: freezed == startedAt
+          ? _value.startedAt
+          : startedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      finishedAt: freezed == finishedAt
+          ? _value.finishedAt
+          : finishedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_PodcastEpisodeDownloadCopyWith<$Res>
+    implements $PodcastEpisodeDownloadCopyWith<$Res> {
+  factory _$$_PodcastEpisodeDownloadCopyWith(_$_PodcastEpisodeDownload value,
+          $Res Function(_$_PodcastEpisodeDownload) then) =
+      __$$_PodcastEpisodeDownloadCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String episodeDisplayTitle,
+      Uri url,
+      String libraryItemId,
+      bool isDownloading,
+      bool isFinished,
+      bool failed,
+      DateTime? startedAt,
+      DateTime createdAt,
+      DateTime? finishedAt});
+}
+
+/// @nodoc
+class __$$_PodcastEpisodeDownloadCopyWithImpl<$Res>
+    extends _$PodcastEpisodeDownloadCopyWithImpl<$Res,
+        _$_PodcastEpisodeDownload>
+    implements _$$_PodcastEpisodeDownloadCopyWith<$Res> {
+  __$$_PodcastEpisodeDownloadCopyWithImpl(_$_PodcastEpisodeDownload _value,
+      $Res Function(_$_PodcastEpisodeDownload) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? episodeDisplayTitle = null,
+    Object? url = null,
+    Object? libraryItemId = null,
+    Object? isDownloading = null,
+    Object? isFinished = null,
+    Object? failed = null,
+    Object? startedAt = freezed,
+    Object? createdAt = null,
+    Object? finishedAt = freezed,
+  }) {
+    return _then(_$_PodcastEpisodeDownload(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      episodeDisplayTitle: null == episodeDisplayTitle
+          ? _value.episodeDisplayTitle
+          : episodeDisplayTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as Uri,
+      libraryItemId: null == libraryItemId
+          ? _value.libraryItemId
+          : libraryItemId // ignore: cast_nullable_to_non_nullable
+              as String,
+      isDownloading: null == isDownloading
+          ? _value.isDownloading
+          : isDownloading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isFinished: null == isFinished
+          ? _value.isFinished
+          : isFinished // ignore: cast_nullable_to_non_nullable
+              as bool,
+      failed: null == failed
+          ? _value.failed
+          : failed // ignore: cast_nullable_to_non_nullable
+              as bool,
+      startedAt: freezed == startedAt
+          ? _value.startedAt
+          : startedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      finishedAt: freezed == finishedAt
+          ? _value.finishedAt
+          : finishedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
+}
+
+/// @nodoc
+
+@jsonConverters
+class _$_PodcastEpisodeDownload implements _PodcastEpisodeDownload {
+  const _$_PodcastEpisodeDownload(
+      {required this.id,
+      required this.episodeDisplayTitle,
+      required this.url,
+      required this.libraryItemId,
+      required this.isDownloading,
+      required this.isFinished,
+      required this.failed,
+      this.startedAt,
+      required this.createdAt,
+      this.finishedAt});
+
+  factory _$_PodcastEpisodeDownload.fromJson(Map<String, dynamic> json) =>
+      _$$_PodcastEpisodeDownloadFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String episodeDisplayTitle;
+  @override
+  final Uri url;
+  @override
+  final String libraryItemId;
+  @override
+  final bool isDownloading;
+  @override
+  final bool isFinished;
+  @override
+  final bool failed;
+  @override
+  final DateTime? startedAt;
+  @override
+  final DateTime createdAt;
+  @override
+  final DateTime? finishedAt;
+
+  @override
+  String toString() {
+    return 'PodcastEpisodeDownload(id: $id, episodeDisplayTitle: $episodeDisplayTitle, url: $url, libraryItemId: $libraryItemId, isDownloading: $isDownloading, isFinished: $isFinished, failed: $failed, startedAt: $startedAt, createdAt: $createdAt, finishedAt: $finishedAt)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_PodcastEpisodeDownload &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.episodeDisplayTitle, episodeDisplayTitle) ||
+                other.episodeDisplayTitle == episodeDisplayTitle) &&
+            (identical(other.url, url) || other.url == url) &&
+            (identical(other.libraryItemId, libraryItemId) ||
+                other.libraryItemId == libraryItemId) &&
+            (identical(other.isDownloading, isDownloading) ||
+                other.isDownloading == isDownloading) &&
+            (identical(other.isFinished, isFinished) ||
+                other.isFinished == isFinished) &&
+            (identical(other.failed, failed) || other.failed == failed) &&
+            (identical(other.startedAt, startedAt) ||
+                other.startedAt == startedAt) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.finishedAt, finishedAt) ||
+                other.finishedAt == finishedAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      episodeDisplayTitle,
+      url,
+      libraryItemId,
+      isDownloading,
+      isFinished,
+      failed,
+      startedAt,
+      createdAt,
+      finishedAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_PodcastEpisodeDownloadCopyWith<_$_PodcastEpisodeDownload> get copyWith =>
+      __$$_PodcastEpisodeDownloadCopyWithImpl<_$_PodcastEpisodeDownload>(
+          this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_PodcastEpisodeDownloadToJson(
+      this,
+    );
+  }
+}
+
+abstract class _PodcastEpisodeDownload implements PodcastEpisodeDownload {
+  const factory _PodcastEpisodeDownload(
+      {required final String id,
+      required final String episodeDisplayTitle,
+      required final Uri url,
+      required final String libraryItemId,
+      required final bool isDownloading,
+      required final bool isFinished,
+      required final bool failed,
+      final DateTime? startedAt,
+      required final DateTime createdAt,
+      final DateTime? finishedAt}) = _$_PodcastEpisodeDownload;
+
+  factory _PodcastEpisodeDownload.fromJson(Map<String, dynamic> json) =
+      _$_PodcastEpisodeDownload.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get episodeDisplayTitle;
+  @override
+  Uri get url;
+  @override
+  String get libraryItemId;
+  @override
+  bool get isDownloading;
+  @override
+  bool get isFinished;
+  @override
+  bool get failed;
+  @override
+  DateTime? get startedAt;
+  @override
+  DateTime get createdAt;
+  @override
+  DateTime? get finishedAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$_PodcastEpisodeDownloadCopyWith<_$_PodcastEpisodeDownload> get copyWith =>
+      throw _privateConstructorUsedError;
+}
