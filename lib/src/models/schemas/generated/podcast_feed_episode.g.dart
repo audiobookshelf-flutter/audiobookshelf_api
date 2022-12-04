d64a6fa8675012941631be2afe1de7b64bdc4193
Paul Nettleton <k9@k9withabone.dev>
2022-12-04 09:46:10 -0600

    Add podcast feed episode

--- lib/src/models/schemas/generated/podcast_feed_episode.g.dart
+++ lib/src/models/schemas/generated/podcast_feed_episode.g.dart
@@ -8,8 +8,39 @@ part of '../podcast_feed_episode.dart';
 
 _$_PodcastFeedEpisode _$$_PodcastFeedEpisodeFromJson(
         Map<String, dynamic> json) =>
-    _$_PodcastFeedEpisode();
+    _$_PodcastFeedEpisode(
+      title: json['title'] as String,
+      subtitle: json['subtitle'] as String,
+      description: json['description'] as String,
+      descriptionPlain: json['descriptionPlain'] as String,
+      pubDate: json['pubDate'] as String,
+      episodeType: json['episodeType'] as String,
+      season: json['season'] as String,
+      episode: json['episode'] as String,
+      author: json['author'] as String,
+      duration: json['duration'] as String,
+      explicit: json['explicit'] as String,
+      publishedAt:
+          const DateTimeEpochConverter().fromJson(json['publishedAt'] as int),
+      enclosure: PodcastEpisodeEnclosure.fromJson(
+          json['enclosure'] as Map<String, dynamic>),
+    );
 
 Map<String, dynamic> _$$_PodcastFeedEpisodeToJson(
         _$_PodcastFeedEpisode instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'title': instance.title,
+      'subtitle': instance.subtitle,
+      'description': instance.description,
+      'descriptionPlain': instance.descriptionPlain,
+      'pubDate': instance.pubDate,
+      'episodeType': instance.episodeType,
+      'season': instance.season,
+      'episode': instance.episode,
+      'author': instance.author,
+      'duration': instance.duration,
+      'explicit': instance.explicit,
+      'publishedAt':
+          const DateTimeEpochConverter().toJson(instance.publishedAt),
+      'enclosure': instance.enclosure,
+    };
--- lib/src/models/schemas/podcast_feed_episode.dart
+++ lib/src/models/schemas/podcast_feed_episode.dart
@@ -1,11 +1,29 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../utils/json_converters.dart';
+import 'podcast_episode_enclosure.dart';
+
 part 'generated/podcast_feed_episode.freezed.dart';
 part 'generated/podcast_feed_episode.g.dart';
 
 @freezed
 class PodcastFeedEpisode with _$PodcastFeedEpisode {
-  const factory PodcastFeedEpisode() = _PodcastFeedEpisode;
+  @jsonConverters
+  const factory PodcastFeedEpisode({
+    required String title,
+    required String subtitle,
+    required String description,
+    required String descriptionPlain,
+    required String pubDate,
+    required String episodeType,
+    required String season,
+    required String episode,
+    required String author,
+    required String duration,
+    required String explicit,
+    required DateTime publishedAt,
+    required PodcastEpisodeEnclosure enclosure,
+  }) = _PodcastFeedEpisode;
 
   factory PodcastFeedEpisode.fromJson(Map<String, dynamic> json) =>
       _$PodcastFeedEpisodeFromJson(json);
