9104dee16a9af611e81ff2fee9aeea4b634067cf
Paul Nettleton <k9@k9withabone.dev>
2022-12-04 10:46:39 -0600

    Add ebook file schema

--- lib/src/models/schemas/generated/ebook_file.freezed.dart
+++ lib/src/models/schemas/generated/ebook_file.freezed.dart
@@ -20,13 +20,31 @@ EBookFile _$EBookFileFromJson(Map<String, dynamic> json) {
 
 /// @nodoc
 mixin _$EBookFile {
+  String get ino => throw _privateConstructorUsedError;
+  FileMetadata get metadata => throw _privateConstructorUsedError;
+  String get ebookFormat => throw _privateConstructorUsedError;
+  DateTime get addedAt => throw _privateConstructorUsedError;
+  DateTime get updatedAt => throw _privateConstructorUsedError;
+
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $EBookFileCopyWith<EBookFile> get copyWith =>
+      throw _privateConstructorUsedError;
 }
 
 /// @nodoc
 abstract class $EBookFileCopyWith<$Res> {
   factory $EBookFileCopyWith(EBookFile value, $Res Function(EBookFile) then) =
       _$EBookFileCopyWithImpl<$Res, EBookFile>;
+  @useResult
+  $Res call(
+      {String ino,
+      FileMetadata metadata,
+      String ebookFormat,
+      DateTime addedAt,
+      DateTime updatedAt});
+
+  $FileMetadataCopyWith<$Res> get metadata;
 }
 
 /// @nodoc
@@ -38,13 +56,65 @@ class _$EBookFileCopyWithImpl<$Res, $Val extends EBookFile>
   final $Val _value;
   // ignore: unused_field
   final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? ino = null,
+    Object? metadata = null,
+    Object? ebookFormat = null,
+    Object? addedAt = null,
+    Object? updatedAt = null,
+  }) {
+    return _then(_value.copyWith(
+      ino: null == ino
+          ? _value.ino
+          : ino // ignore: cast_nullable_to_non_nullable
+              as String,
+      metadata: null == metadata
+          ? _value.metadata
+          : metadata // ignore: cast_nullable_to_non_nullable
+              as FileMetadata,
+      ebookFormat: null == ebookFormat
+          ? _value.ebookFormat
+          : ebookFormat // ignore: cast_nullable_to_non_nullable
+              as String,
+      addedAt: null == addedAt
+          ? _value.addedAt
+          : addedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: null == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $FileMetadataCopyWith<$Res> get metadata {
+    return $FileMetadataCopyWith<$Res>(_value.metadata, (value) {
+      return _then(_value.copyWith(metadata: value) as $Val);
+    });
+  }
 }
 
 /// @nodoc
-abstract class _$$_EBookFileCopyWith<$Res> {
+abstract class _$$_EBookFileCopyWith<$Res> implements $EBookFileCopyWith<$Res> {
   factory _$$_EBookFileCopyWith(
           _$_EBookFile value, $Res Function(_$_EBookFile) then) =
       __$$_EBookFileCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String ino,
+      FileMetadata metadata,
+      String ebookFormat,
+      DateTime addedAt,
+      DateTime updatedAt});
+
+  @override
+  $FileMetadataCopyWith<$Res> get metadata;
 }
 
 /// @nodoc
@@ -54,30 +124,96 @@ class __$$_EBookFileCopyWithImpl<$Res>
   __$$_EBookFileCopyWithImpl(
       _$_EBookFile _value, $Res Function(_$_EBookFile) _then)
       : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? ino = null,
+    Object? metadata = null,
+    Object? ebookFormat = null,
+    Object? addedAt = null,
+    Object? updatedAt = null,
+  }) {
+    return _then(_$_EBookFile(
+      ino: null == ino
+          ? _value.ino
+          : ino // ignore: cast_nullable_to_non_nullable
+              as String,
+      metadata: null == metadata
+          ? _value.metadata
+          : metadata // ignore: cast_nullable_to_non_nullable
+              as FileMetadata,
+      ebookFormat: null == ebookFormat
+          ? _value.ebookFormat
+          : ebookFormat // ignore: cast_nullable_to_non_nullable
+              as String,
+      addedAt: null == addedAt
+          ? _value.addedAt
+          : addedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: null == updatedAt
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
 }
 
 /// @nodoc
-@JsonSerializable()
+
+@jsonConverters
 class _$_EBookFile implements _EBookFile {
-  const _$_EBookFile();
+  const _$_EBookFile(
+      {required this.ino,
+      required this.metadata,
+      required this.ebookFormat,
+      required this.addedAt,
+      required this.updatedAt});
 
   factory _$_EBookFile.fromJson(Map<String, dynamic> json) =>
       _$$_EBookFileFromJson(json);
 
   @override
+  final String ino;
+  @override
+  final FileMetadata metadata;
+  @override
+  final String ebookFormat;
+  @override
+  final DateTime addedAt;
+  @override
+  final DateTime updatedAt;
+
+  @override
   String toString() {
-    return 'EBookFile()';
+    return 'EBookFile(ino: $ino, metadata: $metadata, ebookFormat: $ebookFormat, addedAt: $addedAt, updatedAt: $updatedAt)';
   }
 
   @override
   bool operator ==(dynamic other) {
     return identical(this, other) ||
-        (other.runtimeType == runtimeType && other is _$_EBookFile);
+        (other.runtimeType == runtimeType &&
+            other is _$_EBookFile &&
+            (identical(other.ino, ino) || other.ino == ino) &&
+            (identical(other.metadata, metadata) ||
+                other.metadata == metadata) &&
+            (identical(other.ebookFormat, ebookFormat) ||
+                other.ebookFormat == ebookFormat) &&
+            (identical(other.addedAt, addedAt) || other.addedAt == addedAt) &&
+            (identical(other.updatedAt, updatedAt) ||
+                other.updatedAt == updatedAt));
   }
 
   @JsonKey(ignore: true)
   @override
-  int get hashCode => runtimeType.hashCode;
+  int get hashCode =>
+      Object.hash(runtimeType, ino, metadata, ebookFormat, addedAt, updatedAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_EBookFileCopyWith<_$_EBookFile> get copyWith =>
+      __$$_EBookFileCopyWithImpl<_$_EBookFile>(this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
@@ -88,8 +224,28 @@ class _$_EBookFile implements _EBookFile {
 }
 
 abstract class _EBookFile implements EBookFile {
-  const factory _EBookFile() = _$_EBookFile;
+  const factory _EBookFile(
+      {required final String ino,
+      required final FileMetadata metadata,
+      required final String ebookFormat,
+      required final DateTime addedAt,
+      required final DateTime updatedAt}) = _$_EBookFile;
 
   factory _EBookFile.fromJson(Map<String, dynamic> json) =
       _$_EBookFile.fromJson;
+
+  @override
+  String get ino;
+  @override
+  FileMetadata get metadata;
+  @override
+  String get ebookFormat;
+  @override
+  DateTime get addedAt;
+  @override
+  DateTime get updatedAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$_EBookFileCopyWith<_$_EBookFile> get copyWith =>
+      throw _privateConstructorUsedError;
 }
--- lib/src/models/schemas/generated/ebook_file.g.dart
+++ lib/src/models/schemas/generated/ebook_file.g.dart
@@ -6,7 +6,20 @@ part of '../ebook_file.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_EBookFile _$$_EBookFileFromJson(Map<String, dynamic> json) => _$_EBookFile();
+_$_EBookFile _$$_EBookFileFromJson(Map<String, dynamic> json) => _$_EBookFile(
+      ino: json['ino'] as String,
+      metadata: FileMetadata.fromJson(json['metadata'] as Map<String, dynamic>),
+      ebookFormat: json['ebookFormat'] as String,
+      addedAt: const DateTimeEpochConverter().fromJson(json['addedAt'] as int),
+      updatedAt:
+          const DateTimeEpochConverter().fromJson(json['updatedAt'] as int),
+    );
 
 Map<String, dynamic> _$$_EBookFileToJson(_$_EBookFile instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'ino': instance.ino,
+      'metadata': instance.metadata,
+      'ebookFormat': instance.ebookFormat,
+      'addedAt': const DateTimeEpochConverter().toJson(instance.addedAt),
+      'updatedAt': const DateTimeEpochConverter().toJson(instance.updatedAt),
+    };
