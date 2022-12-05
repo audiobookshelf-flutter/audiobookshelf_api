b312860972a85d287b358040454be3cce252bcb2
Paul Nettleton <k9@k9withabone.dev>
2022-12-05 05:04:28 -0600

    Add backup schema

---
+++ lib/src/models/schemas/generated/backup.freezed.dart
@@ -0,0 +1,349 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of '../backup.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Backup _$BackupFromJson(Map<String, dynamic> json) {
+  return _Backup.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Backup {
+  String get id => throw _privateConstructorUsedError;
+  bool get backupMetadataCovers => throw _privateConstructorUsedError;
+  String get backupDirPath => throw _privateConstructorUsedError;
+  String get datePretty => throw _privateConstructorUsedError;
+  String get fullPath => throw _privateConstructorUsedError;
+  String get path => throw _privateConstructorUsedError;
+  String get filename => throw _privateConstructorUsedError;
+  int get fileSize => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  String get serverVersion => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $BackupCopyWith<Backup> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BackupCopyWith<$Res> {
+  factory $BackupCopyWith(Backup value, $Res Function(Backup) then) =
+      _$BackupCopyWithImpl<$Res, Backup>;
+  @useResult
+  $Res call(
+      {String id,
+      bool backupMetadataCovers,
+      String backupDirPath,
+      String datePretty,
+      String fullPath,
+      String path,
+      String filename,
+      int fileSize,
+      DateTime createdAt,
+      String serverVersion});
+}
+
+/// @nodoc
+class _$BackupCopyWithImpl<$Res, $Val extends Backup>
+    implements $BackupCopyWith<$Res> {
+  _$BackupCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? backupMetadataCovers = null,
+    Object? backupDirPath = null,
+    Object? datePretty = null,
+    Object? fullPath = null,
+    Object? path = null,
+    Object? filename = null,
+    Object? fileSize = null,
+    Object? createdAt = null,
+    Object? serverVersion = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      backupMetadataCovers: null == backupMetadataCovers
+          ? _value.backupMetadataCovers
+          : backupMetadataCovers // ignore: cast_nullable_to_non_nullable
+              as bool,
+      backupDirPath: null == backupDirPath
+          ? _value.backupDirPath
+          : backupDirPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      datePretty: null == datePretty
+          ? _value.datePretty
+          : datePretty // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullPath: null == fullPath
+          ? _value.fullPath
+          : fullPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      path: null == path
+          ? _value.path
+          : path // ignore: cast_nullable_to_non_nullable
+              as String,
+      filename: null == filename
+          ? _value.filename
+          : filename // ignore: cast_nullable_to_non_nullable
+              as String,
+      fileSize: null == fileSize
+          ? _value.fileSize
+          : fileSize // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      serverVersion: null == serverVersion
+          ? _value.serverVersion
+          : serverVersion // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_BackupCopyWith<$Res> implements $BackupCopyWith<$Res> {
+  factory _$$_BackupCopyWith(_$_Backup value, $Res Function(_$_Backup) then) =
+      __$$_BackupCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      bool backupMetadataCovers,
+      String backupDirPath,
+      String datePretty,
+      String fullPath,
+      String path,
+      String filename,
+      int fileSize,
+      DateTime createdAt,
+      String serverVersion});
+}
+
+/// @nodoc
+class __$$_BackupCopyWithImpl<$Res>
+    extends _$BackupCopyWithImpl<$Res, _$_Backup>
+    implements _$$_BackupCopyWith<$Res> {
+  __$$_BackupCopyWithImpl(_$_Backup _value, $Res Function(_$_Backup) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? backupMetadataCovers = null,
+    Object? backupDirPath = null,
+    Object? datePretty = null,
+    Object? fullPath = null,
+    Object? path = null,
+    Object? filename = null,
+    Object? fileSize = null,
+    Object? createdAt = null,
+    Object? serverVersion = null,
+  }) {
+    return _then(_$_Backup(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      backupMetadataCovers: null == backupMetadataCovers
+          ? _value.backupMetadataCovers
+          : backupMetadataCovers // ignore: cast_nullable_to_non_nullable
+              as bool,
+      backupDirPath: null == backupDirPath
+          ? _value.backupDirPath
+          : backupDirPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      datePretty: null == datePretty
+          ? _value.datePretty
+          : datePretty // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullPath: null == fullPath
+          ? _value.fullPath
+          : fullPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      path: null == path
+          ? _value.path
+          : path // ignore: cast_nullable_to_non_nullable
+              as String,
+      filename: null == filename
+          ? _value.filename
+          : filename // ignore: cast_nullable_to_non_nullable
+              as String,
+      fileSize: null == fileSize
+          ? _value.fileSize
+          : fileSize // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      serverVersion: null == serverVersion
+          ? _value.serverVersion
+          : serverVersion // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+@jsonConverters
+class _$_Backup implements _Backup {
+  const _$_Backup(
+      {required this.id,
+      required this.backupMetadataCovers,
+      required this.backupDirPath,
+      required this.datePretty,
+      required this.fullPath,
+      required this.path,
+      required this.filename,
+      required this.fileSize,
+      required this.createdAt,
+      required this.serverVersion});
+
+  factory _$_Backup.fromJson(Map<String, dynamic> json) =>
+      _$$_BackupFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final bool backupMetadataCovers;
+  @override
+  final String backupDirPath;
+  @override
+  final String datePretty;
+  @override
+  final String fullPath;
+  @override
+  final String path;
+  @override
+  final String filename;
+  @override
+  final int fileSize;
+  @override
+  final DateTime createdAt;
+  @override
+  final String serverVersion;
+
+  @override
+  String toString() {
+    return 'Backup(id: $id, backupMetadataCovers: $backupMetadataCovers, backupDirPath: $backupDirPath, datePretty: $datePretty, fullPath: $fullPath, path: $path, filename: $filename, fileSize: $fileSize, createdAt: $createdAt, serverVersion: $serverVersion)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Backup &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.backupMetadataCovers, backupMetadataCovers) ||
+                other.backupMetadataCovers == backupMetadataCovers) &&
+            (identical(other.backupDirPath, backupDirPath) ||
+                other.backupDirPath == backupDirPath) &&
+            (identical(other.datePretty, datePretty) ||
+                other.datePretty == datePretty) &&
+            (identical(other.fullPath, fullPath) ||
+                other.fullPath == fullPath) &&
+            (identical(other.path, path) || other.path == path) &&
+            (identical(other.filename, filename) ||
+                other.filename == filename) &&
+            (identical(other.fileSize, fileSize) ||
+                other.fileSize == fileSize) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.serverVersion, serverVersion) ||
+                other.serverVersion == serverVersion));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      backupMetadataCovers,
+      backupDirPath,
+      datePretty,
+      fullPath,
+      path,
+      filename,
+      fileSize,
+      createdAt,
+      serverVersion);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_BackupCopyWith<_$_Backup> get copyWith =>
+      __$$_BackupCopyWithImpl<_$_Backup>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_BackupToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Backup implements Backup {
+  const factory _Backup(
+      {required final String id,
+      required final bool backupMetadataCovers,
+      required final String backupDirPath,
+      required final String datePretty,
+      required final String fullPath,
+      required final String path,
+      required final String filename,
+      required final int fileSize,
+      required final DateTime createdAt,
+      required final String serverVersion}) = _$_Backup;
+
+  factory _Backup.fromJson(Map<String, dynamic> json) = _$_Backup.fromJson;
+
+  @override
+  String get id;
+  @override
+  bool get backupMetadataCovers;
+  @override
+  String get backupDirPath;
+  @override
+  String get datePretty;
+  @override
+  String get fullPath;
+  @override
+  String get path;
+  @override
+  String get filename;
+  @override
+  int get fileSize;
+  @override
+  DateTime get createdAt;
+  @override
+  String get serverVersion;
+  @override
+  @JsonKey(ignore: true)
+  _$$_BackupCopyWith<_$_Backup> get copyWith =>
+      throw _privateConstructorUsedError;
+}
