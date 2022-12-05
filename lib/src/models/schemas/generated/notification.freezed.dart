a176ee0d1cd5e48114f5339801da87c7b9e94be3
Paul Nettleton <k9@k9withabone.dev>
2022-12-05 05:38:32 -0600

    Add notification schema

--- lib/src/models/schemas/generated/notification.freezed.dart
+++ lib/src/models/schemas/generated/notification.freezed.dart
@@ -20,7 +20,24 @@ Notification _$NotificationFromJson(Map<String, dynamic> json) {
 
 /// @nodoc
 mixin _$Notification {
+  String get id => throw _privateConstructorUsedError;
+  String? get libraryId => throw _privateConstructorUsedError;
+  String get eventName => throw _privateConstructorUsedError;
+  List<Uri> get urls => throw _privateConstructorUsedError;
+  String get titleTemplate => throw _privateConstructorUsedError;
+  String get bodyTemplate => throw _privateConstructorUsedError;
+  bool get enabled => throw _privateConstructorUsedError;
+  String get type => throw _privateConstructorUsedError;
+  DateTime? get lastFiredAt => throw _privateConstructorUsedError;
+  bool get lastAttemptFailed => throw _privateConstructorUsedError;
+  int get numConsecutiveFailedAttempts => throw _privateConstructorUsedError;
+  int get numTimesFired => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $NotificationCopyWith<Notification> get copyWith =>
+      throw _privateConstructorUsedError;
 }
 
 /// @nodoc
@@ -28,6 +45,21 @@ abstract class $NotificationCopyWith<$Res> {
   factory $NotificationCopyWith(
           Notification value, $Res Function(Notification) then) =
       _$NotificationCopyWithImpl<$Res, Notification>;
+  @useResult
+  $Res call(
+      {String id,
+      String? libraryId,
+      String eventName,
+      List<Uri> urls,
+      String titleTemplate,
+      String bodyTemplate,
+      bool enabled,
+      String type,
+      DateTime? lastFiredAt,
+      bool lastAttemptFailed,
+      int numConsecutiveFailedAttempts,
+      int numTimesFired,
+      DateTime createdAt});
 }
 
 /// @nodoc
@@ -39,13 +71,103 @@ class _$NotificationCopyWithImpl<$Res, $Val extends Notification>
   final $Val _value;
   // ignore: unused_field
   final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? libraryId = freezed,
+    Object? eventName = null,
+    Object? urls = null,
+    Object? titleTemplate = null,
+    Object? bodyTemplate = null,
+    Object? enabled = null,
+    Object? type = null,
+    Object? lastFiredAt = freezed,
+    Object? lastAttemptFailed = null,
+    Object? numConsecutiveFailedAttempts = null,
+    Object? numTimesFired = null,
+    Object? createdAt = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      libraryId: freezed == libraryId
+          ? _value.libraryId
+          : libraryId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      eventName: null == eventName
+          ? _value.eventName
+          : eventName // ignore: cast_nullable_to_non_nullable
+              as String,
+      urls: null == urls
+          ? _value.urls
+          : urls // ignore: cast_nullable_to_non_nullable
+              as List<Uri>,
+      titleTemplate: null == titleTemplate
+          ? _value.titleTemplate
+          : titleTemplate // ignore: cast_nullable_to_non_nullable
+              as String,
+      bodyTemplate: null == bodyTemplate
+          ? _value.bodyTemplate
+          : bodyTemplate // ignore: cast_nullable_to_non_nullable
+              as String,
+      enabled: null == enabled
+          ? _value.enabled
+          : enabled // ignore: cast_nullable_to_non_nullable
+              as bool,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String,
+      lastFiredAt: freezed == lastFiredAt
+          ? _value.lastFiredAt
+          : lastFiredAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      lastAttemptFailed: null == lastAttemptFailed
+          ? _value.lastAttemptFailed
+          : lastAttemptFailed // ignore: cast_nullable_to_non_nullable
+              as bool,
+      numConsecutiveFailedAttempts: null == numConsecutiveFailedAttempts
+          ? _value.numConsecutiveFailedAttempts
+          : numConsecutiveFailedAttempts // ignore: cast_nullable_to_non_nullable
+              as int,
+      numTimesFired: null == numTimesFired
+          ? _value.numTimesFired
+          : numTimesFired // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
 }
 
 /// @nodoc
-abstract class _$$_NotificationCopyWith<$Res> {
+abstract class _$$_NotificationCopyWith<$Res>
+    implements $NotificationCopyWith<$Res> {
   factory _$$_NotificationCopyWith(
           _$_Notification value, $Res Function(_$_Notification) then) =
       __$$_NotificationCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String? libraryId,
+      String eventName,
+      List<Uri> urls,
+      String titleTemplate,
+      String bodyTemplate,
+      bool enabled,
+      String type,
+      DateTime? lastFiredAt,
+      bool lastAttemptFailed,
+      int numConsecutiveFailedAttempts,
+      int numTimesFired,
+      DateTime createdAt});
 }
 
 /// @nodoc
@@ -55,30 +177,196 @@ class __$$_NotificationCopyWithImpl<$Res>
   __$$_NotificationCopyWithImpl(
       _$_Notification _value, $Res Function(_$_Notification) _then)
       : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? libraryId = freezed,
+    Object? eventName = null,
+    Object? urls = null,
+    Object? titleTemplate = null,
+    Object? bodyTemplate = null,
+    Object? enabled = null,
+    Object? type = null,
+    Object? lastFiredAt = freezed,
+    Object? lastAttemptFailed = null,
+    Object? numConsecutiveFailedAttempts = null,
+    Object? numTimesFired = null,
+    Object? createdAt = null,
+  }) {
+    return _then(_$_Notification(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      libraryId: freezed == libraryId
+          ? _value.libraryId
+          : libraryId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      eventName: null == eventName
+          ? _value.eventName
+          : eventName // ignore: cast_nullable_to_non_nullable
+              as String,
+      urls: null == urls
+          ? _value._urls
+          : urls // ignore: cast_nullable_to_non_nullable
+              as List<Uri>,
+      titleTemplate: null == titleTemplate
+          ? _value.titleTemplate
+          : titleTemplate // ignore: cast_nullable_to_non_nullable
+              as String,
+      bodyTemplate: null == bodyTemplate
+          ? _value.bodyTemplate
+          : bodyTemplate // ignore: cast_nullable_to_non_nullable
+              as String,
+      enabled: null == enabled
+          ? _value.enabled
+          : enabled // ignore: cast_nullable_to_non_nullable
+              as bool,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String,
+      lastFiredAt: freezed == lastFiredAt
+          ? _value.lastFiredAt
+          : lastFiredAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      lastAttemptFailed: null == lastAttemptFailed
+          ? _value.lastAttemptFailed
+          : lastAttemptFailed // ignore: cast_nullable_to_non_nullable
+              as bool,
+      numConsecutiveFailedAttempts: null == numConsecutiveFailedAttempts
+          ? _value.numConsecutiveFailedAttempts
+          : numConsecutiveFailedAttempts // ignore: cast_nullable_to_non_nullable
+              as int,
+      numTimesFired: null == numTimesFired
+          ? _value.numTimesFired
+          : numTimesFired // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
 }
 
 /// @nodoc
-@JsonSerializable()
+
+@jsonConverters
 class _$_Notification implements _Notification {
-  const _$_Notification();
+  const _$_Notification(
+      {required this.id,
+      this.libraryId,
+      required this.eventName,
+      required final List<Uri> urls,
+      required this.titleTemplate,
+      required this.bodyTemplate,
+      required this.enabled,
+      required this.type,
+      this.lastFiredAt,
+      required this.lastAttemptFailed,
+      required this.numConsecutiveFailedAttempts,
+      required this.numTimesFired,
+      required this.createdAt})
+      : _urls = urls;
 
   factory _$_Notification.fromJson(Map<String, dynamic> json) =>
       _$$_NotificationFromJson(json);
 
   @override
+  final String id;
+  @override
+  final String? libraryId;
+  @override
+  final String eventName;
+  final List<Uri> _urls;
+  @override
+  List<Uri> get urls {
+    if (_urls is EqualUnmodifiableListView) return _urls;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_urls);
+  }
+
+  @override
+  final String titleTemplate;
+  @override
+  final String bodyTemplate;
+  @override
+  final bool enabled;
+  @override
+  final String type;
+  @override
+  final DateTime? lastFiredAt;
+  @override
+  final bool lastAttemptFailed;
+  @override
+  final int numConsecutiveFailedAttempts;
+  @override
+  final int numTimesFired;
+  @override
+  final DateTime createdAt;
+
+  @override
   String toString() {
-    return 'Notification()';
+    return 'Notification(id: $id, libraryId: $libraryId, eventName: $eventName, urls: $urls, titleTemplate: $titleTemplate, bodyTemplate: $bodyTemplate, enabled: $enabled, type: $type, lastFiredAt: $lastFiredAt, lastAttemptFailed: $lastAttemptFailed, numConsecutiveFailedAttempts: $numConsecutiveFailedAttempts, numTimesFired: $numTimesFired, createdAt: $createdAt)';
   }
 
   @override
   bool operator ==(dynamic other) {
     return identical(this, other) ||
-        (other.runtimeType == runtimeType && other is _$_Notification);
+        (other.runtimeType == runtimeType &&
+            other is _$_Notification &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.libraryId, libraryId) ||
+                other.libraryId == libraryId) &&
+            (identical(other.eventName, eventName) ||
+                other.eventName == eventName) &&
+            const DeepCollectionEquality().equals(other._urls, _urls) &&
+            (identical(other.titleTemplate, titleTemplate) ||
+                other.titleTemplate == titleTemplate) &&
+            (identical(other.bodyTemplate, bodyTemplate) ||
+                other.bodyTemplate == bodyTemplate) &&
+            (identical(other.enabled, enabled) || other.enabled == enabled) &&
+            (identical(other.type, type) || other.type == type) &&
+            (identical(other.lastFiredAt, lastFiredAt) ||
+                other.lastFiredAt == lastFiredAt) &&
+            (identical(other.lastAttemptFailed, lastAttemptFailed) ||
+                other.lastAttemptFailed == lastAttemptFailed) &&
+            (identical(other.numConsecutiveFailedAttempts,
+                    numConsecutiveFailedAttempts) ||
+                other.numConsecutiveFailedAttempts ==
+                    numConsecutiveFailedAttempts) &&
+            (identical(other.numTimesFired, numTimesFired) ||
+                other.numTimesFired == numTimesFired) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt));
   }
 
   @JsonKey(ignore: true)
   @override
-  int get hashCode => runtimeType.hashCode;
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      libraryId,
+      eventName,
+      const DeepCollectionEquality().hash(_urls),
+      titleTemplate,
+      bodyTemplate,
+      enabled,
+      type,
+      lastFiredAt,
+      lastAttemptFailed,
+      numConsecutiveFailedAttempts,
+      numTimesFired,
+      createdAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_NotificationCopyWith<_$_Notification> get copyWith =>
+      __$$_NotificationCopyWithImpl<_$_Notification>(this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
@@ -89,8 +377,52 @@ class _$_Notification implements _Notification {
 }
 
 abstract class _Notification implements Notification {
-  const factory _Notification() = _$_Notification;
+  const factory _Notification(
+      {required final String id,
+      final String? libraryId,
+      required final String eventName,
+      required final List<Uri> urls,
+      required final String titleTemplate,
+      required final String bodyTemplate,
+      required final bool enabled,
+      required final String type,
+      final DateTime? lastFiredAt,
+      required final bool lastAttemptFailed,
+      required final int numConsecutiveFailedAttempts,
+      required final int numTimesFired,
+      required final DateTime createdAt}) = _$_Notification;
 
   factory _Notification.fromJson(Map<String, dynamic> json) =
       _$_Notification.fromJson;
+
+  @override
+  String get id;
+  @override
+  String? get libraryId;
+  @override
+  String get eventName;
+  @override
+  List<Uri> get urls;
+  @override
+  String get titleTemplate;
+  @override
+  String get bodyTemplate;
+  @override
+  bool get enabled;
+  @override
+  String get type;
+  @override
+  DateTime? get lastFiredAt;
+  @override
+  bool get lastAttemptFailed;
+  @override
+  int get numConsecutiveFailedAttempts;
+  @override
+  int get numTimesFired;
+  @override
+  DateTime get createdAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$_NotificationCopyWith<_$_Notification> get copyWith =>
+      throw _privateConstructorUsedError;
 }
--- lib/src/models/schemas/generated/notification.g.dart
+++ lib/src/models/schemas/generated/notification.g.dart
@@ -7,7 +7,52 @@ part of '../notification.dart';
 // **************************************************************************
 
 _$_Notification _$$_NotificationFromJson(Map<String, dynamic> json) =>
-    _$_Notification();
+    _$_Notification(
+      id: json['id'] as String,
+      libraryId: json['libraryId'] as String?,
+      eventName: json['eventName'] as String,
+      urls: (json['urls'] as List<dynamic>)
+          .map((e) => Uri.parse(e as String))
+          .toList(),
+      titleTemplate: json['titleTemplate'] as String,
+      bodyTemplate: json['bodyTemplate'] as String,
+      enabled: json['enabled'] as bool,
+      type: json['type'] as String,
+      lastFiredAt: _$JsonConverterFromJson<int, DateTime>(
+          json['lastFiredAt'], const DateTimeEpochConverter().fromJson),
+      lastAttemptFailed: json['lastAttemptFailed'] as bool,
+      numConsecutiveFailedAttempts: json['numConsecutiveFailedAttempts'] as int,
+      numTimesFired: json['numTimesFired'] as int,
+      createdAt:
+          const DateTimeEpochConverter().fromJson(json['createdAt'] as int),
+    );
 
 Map<String, dynamic> _$$_NotificationToJson(_$_Notification instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'id': instance.id,
+      'libraryId': instance.libraryId,
+      'eventName': instance.eventName,
+      'urls': instance.urls.map((e) => e.toString()).toList(),
+      'titleTemplate': instance.titleTemplate,
+      'bodyTemplate': instance.bodyTemplate,
+      'enabled': instance.enabled,
+      'type': instance.type,
+      'lastFiredAt': _$JsonConverterToJson<int, DateTime>(
+          instance.lastFiredAt, const DateTimeEpochConverter().toJson),
+      'lastAttemptFailed': instance.lastAttemptFailed,
+      'numConsecutiveFailedAttempts': instance.numConsecutiveFailedAttempts,
+      'numTimesFired': instance.numTimesFired,
+      'createdAt': const DateTimeEpochConverter().toJson(instance.createdAt),
+    };
+
+Value? _$JsonConverterFromJson<Json, Value>(
+  Object? json,
+  Value? Function(Json json) fromJson,
+) =>
+    json == null ? null : fromJson(json as Json);
+
+Json? _$JsonConverterToJson<Json, Value>(
+  Value? value,
+  Json? Function(Value value) toJson,
+) =>
+    value == null ? null : toJson(value);
