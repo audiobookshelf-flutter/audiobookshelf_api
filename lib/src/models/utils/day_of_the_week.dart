bf430925411a1114ef06e501c82d5e444de05533
Paul Nettleton <k9@k9withabone.dev>
2022-12-05 11:01:11 -0600

    Add cron expression

---
+++ lib/src/models/utils/day_of_the_week.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonEnum(valueField: 'value')
+enum DayOfTheWeek {
+  sunday(0, 'Sunday'),
+  monday(1, 'Monday'),
+  tuesday(2, 'Tuesday'),
+  wednesday(3, 'Wednesday'),
+  thursday(4, 'Thursday'),
+  friday(5, 'Friday'),
+  saturday(6, 'Saturday');
+
+  static final byValue = {
+    for (final value in DayOfTheWeek.values) value.value: value
+  };
+
+  final int value;
+  final String name;
+
+  const DayOfTheWeek(this.value, this.name);
+}
