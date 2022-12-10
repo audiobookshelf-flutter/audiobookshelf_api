c7f86ef9136cd13822930ba1e3a69a2188db5e51
Paul Nettleton <k9@k9withabone.dev>
2022-12-10 12:44:50 -0600

    Add get all backups

--- lib/src/services/backups_service.dart
+++ lib/src/services/backups_service.dart
@@ -1,5 +1,23 @@
+import '../models/schemas/backup.dart';
+import '../utils/from_json.dart';
+import '../utils/typedefs.dart';
 import 'service.dart';
 
 class BackupsService extends Service {
+  /// `/api/backups`
+  static const basePath = '${Service.basePath}/backups';
+
   const BackupsService(super.api);
+
+  /// See [Get All Backups](https://api.audiobookshelf.org/#get-all-backups)
+  Future<List<Backup>?> getAll({
+    ResponseErrorHandler? responseErrorHandler,
+  }) {
+    return api.getJson(
+      path: basePath,
+      requiresAuth: true,
+      responseErrorHandler: responseErrorHandler,
+      fromJson: (json) => listFromJsonKey(json, 'backups', Backup.fromJson),
+    );
+  }
 }
