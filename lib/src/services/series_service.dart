b3b5d6bf9ce57282284d7e0513891bd545c7aaf3
Paul Nettleton <k9@k9withabone.dev>
2022-12-10 14:24:08 -0600

    Add search for series

--- lib/src/services/series_service.dart
+++ lib/src/services/series_service.dart
@@ -1,5 +1,26 @@
+import '../models/request_parameters/search_req_params.dart';
+import '../models/schemas/series.dart';
+import '../utils/from_json.dart';
+import '../utils/typedefs.dart';
 import 'service.dart';
 
 class SeriesService extends Service {
+  /// `/api/series`
+  static const basePath = '${Service.basePath}/series';
+
   const SeriesService(super.api);
+
+  /// See [Search for Series](https://api.audiobookshelf.org/#search-for-series)
+  Future<List<Series>?> search({
+    required SearchReqParams parameters,
+    ResponseErrorHandler? responseErrorHandler,
+  }) {
+    return api.getJson(
+      path: '$basePath/search',
+      queryParameters: parameters.toJson(),
+      requiresAuth: true,
+      responseErrorHandler: responseErrorHandler,
+      fromJson: (json) => listFromJson(json, Series.fromJson),
+    );
+  }
 }
