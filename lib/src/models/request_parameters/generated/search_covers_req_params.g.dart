93eac2f6c46aba9c7468513572a0ef4a363cde42
Paul Nettleton <k9@k9withabone.dev>
2022-12-11 15:46:03 -0600

    Add search for covers

---
+++ lib/src/models/request_parameters/generated/search_covers_req_params.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../search_covers_req_params.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$SearchCoversReqParamsToJson(
+    SearchCoversReqParams instance) {
+  final val = <String, dynamic>{
+    'podcast': const BoolBinaryConverter().toJson(instance.podcast),
+    'title': instance.title,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('author', instance.author);
+  val['provider'] = _$MetadataProviderEnumMap[instance.provider]!;
+  return val;
+}
+
+const _$MetadataProviderEnumMap = {
+  MetadataProvider.google: 'google',
+  MetadataProvider.openLibrary: 'openlibrary',
+  MetadataProvider.itunes: 'itunes',
+  MetadataProvider.audible: 'audible',
+  MetadataProvider.audibleCa: 'audible.ca',
+  MetadataProvider.audibleUk: 'audible.uk',
+  MetadataProvider.audibleAu: 'audible.au',
+  MetadataProvider.audibleFr: 'audible.fr',
+  MetadataProvider.audibleDe: 'audible.de',
+  MetadataProvider.audibleJp: 'audible.jp',
+  MetadataProvider.audibleIt: 'audible.it',
+  MetadataProvider.audibleIn: 'audible.in',
+  MetadataProvider.audibleEs: 'audible.es',
+};
--- lib/src/services/search_service.dart
+++ lib/src/services/search_service.dart
@@ -1,5 +1,27 @@
+import '../models/request_parameters/search_covers_req_params.dart';
+import '../utils/typedefs.dart';
 import 'service.dart';
 
 class SearchService extends Service {
+  /// `/api/search`
+  static const basePath = '${Service.basePath}/search';
+
   const SearchService(super.api);
+
+  /// See [Search for Covers](https://api.audiobookshelf.org/#search-for-covers)
+  Future<List<Uri>?> covers({
+    required SearchCoversReqParams parameters,
+    ResponseErrorHandler? responseErrorHandler,
+  }) {
+    return api.getJson(
+      path: '$basePath/covers',
+      queryParameters: parameters.toJson(),
+      requiresAuth: true,
+      responseErrorHandler: responseErrorHandler,
+      fromJson: (json) {
+        final urls = json as List<dynamic>;
+        return urls.cast<String>().map((e) => Uri.parse(e)).toList();
+      },
+    );
+  }
 }
