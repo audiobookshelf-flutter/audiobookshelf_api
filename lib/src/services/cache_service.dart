5168e81c0a2b4c1d41cc6c5ab3078071e655e7d2
Paul Nettleton <k9@k9withabone.dev>
2022-12-11 17:35:55 -0600

    Add purge all cache

--- lib/src/services/cache_service.dart
+++ lib/src/services/cache_service.dart
@@ -1,5 +1,20 @@
+import '../utils/typedefs.dart';
 import 'service.dart';
 
 class CacheService extends Service {
+  /// `/api/cache`
+  static const basePath = '${Service.basePath}/cache';
+
   const CacheService(super.api);
+
+  /// See [Purge All Cache](https://api.audiobookshelf.org/#purge-all-cache)
+  Future<void> purgeAll({
+    ResponseErrorHandler? responseErrorHandler,
+  }) {
+    return api.post(
+      path: '$basePath/purge',
+      requiresAuth: true,
+      responseErrorHandler: responseErrorHandler,
+    );
+  }
 }
