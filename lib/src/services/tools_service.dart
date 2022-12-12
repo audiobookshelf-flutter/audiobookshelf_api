1637090d38e1a46d7e785d60b92a76b0c8ddf4f9
Paul Nettleton <k9@k9withabone.dev>
2022-12-11 18:28:56 -0600

    Add encode a book as m4b

--- lib/src/services/tools_service.dart
+++ lib/src/services/tools_service.dart
@@ -1,5 +1,21 @@
+import '../utils/typedefs.dart';
 import 'service.dart';
 
 class ToolsService extends Service {
+  /// `/api/tools`
+  static const basePath = '${Service.basePath}/tools';
+
   const ToolsService(super.api);
+
+  /// See [Encode a Book as M4B](https://api.audiobookshelf.org/#encode-a-book-as-m4b)
+  Future<void> encodeM4b({
+    required String libraryItemId,
+    ResponseErrorHandler? responseErrorHandler,
+  }) {
+    return api.post(
+      path: '$basePath/item/$libraryItemId/encode-m4b',
+      requiresAuth: true,
+      responseErrorHandler: responseErrorHandler,
+    );
+  }
 }
