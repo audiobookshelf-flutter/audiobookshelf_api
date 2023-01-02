35c49a40b3bbb17def27cea2f05e716533474376
Paul Nettleton <k9@k9withabone.dev>
2023-01-02 05:32:05 -0600

    Add CreateUserReqParams tests

---
+++ test/models/request_parameters/create_user_req_params_test.dart
@@ -0,0 +1,99 @@
+import 'package:audiobookshelf_api/audiobookshelf_api.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CreateUserReqParams', () {
+    const username = 'username';
+    const password = 'password';
+    const type = UserType.admin;
+    const seriesHideFromContinueListening = ['seriesId'];
+    const isActive = false;
+    const isLocked = true;
+    const librariesAccessible = ['libraryId'];
+    const itemTagsAccessible = ['tag'];
+
+    final mockMediaProgress = MockMediaProgress();
+    final mediaProgress = [mockMediaProgress];
+    final mockBookmark = MockAudioBookmark();
+    final bookmarks = [mockBookmark];
+    final lastSeen = DateTime.fromMillisecondsSinceEpoch(0);
+    final createdAt = DateTime.fromMillisecondsSinceEpoch(0);
+    final mockPermissions = MockCreateUserPermissionsReqParams();
+
+    late CreateUserReqParams sut;
+
+    setUp(() {
+      sut = CreateUserReqParams(
+        username: username,
+        password: password,
+        type: type,
+        mediaProgress: mediaProgress,
+        bookmarks: bookmarks,
+        seriesHideFromContinueListening: seriesHideFromContinueListening,
+        isActive: isActive,
+        isLocked: isLocked,
+        lastSeen: lastSeen,
+        createdAt: createdAt,
+        permissions: mockPermissions,
+        librariesAccessible: librariesAccessible,
+        itemTagsAccessible: itemTagsAccessible,
+      );
+    });
+
+    tearDown(() {
+      reset(mockMediaProgress);
+      reset(mockBookmark);
+      reset(mockPermissions);
+    });
+
+    group('toJson', () {
+      test('toJson', () {
+        const testMap = {'test': 'test'};
+        when(() => mockMediaProgress.toJson()).thenReturn(testMap);
+        when(() => mockBookmark.toJson()).thenReturn(testMap);
+        when(() => mockPermissions.toJson()).thenReturn(testMap);
+        expect(sut.toJson(), {
+          'username': username,
+          'password': password,
+          'type': type.name,
+          'mediaProgress': [testMap],
+          'bookmarks': [testMap],
+          'seriesHideFromContinueListening': seriesHideFromContinueListening,
+          'isActive': isActive,
+          'isLocked': isLocked,
+          'lastSeen': 0,
+          'createdAt': 0,
+          'permissions': testMap,
+          'librariesAccessible': librariesAccessible,
+          'itemTagsAccessible': itemTagsAccessible,
+        });
+        verify(() => mockMediaProgress.toJson()).called(1);
+        verify(() => mockBookmark.toJson()).called(1);
+        verify(() => mockPermissions.toJson()).called(1);
+      });
+
+      test('remove defaults', () {
+        expect(
+          const CreateUserReqParams(
+            username: username,
+            password: password,
+            type: type,
+          ).toJson(),
+          {
+            'username': username,
+            'password': password,
+            'type': type.name,
+          },
+        );
+      });
+    });
+  });
+}
+
+class MockMediaProgress extends Mock implements MediaProgress {}
+
+class MockAudioBookmark extends Mock implements AudioBookmark {}
+
+class MockCreateUserPermissionsReqParams extends Mock
+    implements CreateUserPermissionsReqParams {}
