d5bcd85d536fe6a02e3a077f3b04c708701bb78d
Paul Nettleton <k9@k9withabone.dev>
2023-01-03 01:45:34 -0600

    Add GetLibrarysItemsReqParams tests

---
+++ test/models/request_parameters/get_librarys_items_req_params_test.dart
@@ -0,0 +1,55 @@
+import 'package:audiobookshelf_api/audiobookshelf_api.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+const _filter = 'filter';
+
+void main() {
+  group('GetLibrarysItemsReqParams', () {
+    const limit = 1;
+    const page = 1;
+    const sort = 'sort';
+    const desc = true;
+    const minified = true;
+    const collapseSeries = true;
+
+    final fakeFilter = FakeFilter();
+
+    late GetLibrarysItemsReqParams sut;
+
+    setUp(() {
+      sut = GetLibrarysItemsReqParams(
+        limit: limit,
+        page: page,
+        sort: sort,
+        desc: desc,
+        filter: fakeFilter,
+        minified: minified,
+        collapseSeries: collapseSeries,
+      );
+    });
+
+    group('toJson', () {
+      test('toJson', () {
+        expect(sut.toJson(), const {
+          'limit': limit,
+          'page': page,
+          'sort': sort,
+          'desc': 1,
+          'filter': _filter,
+          'minified': 1,
+          'collapseseries': 1,
+        });
+      });
+
+      test('remove defaults', () {
+        expect(const GetLibrarysItemsReqParams().toJson(), isNull);
+      });
+    });
+  });
+}
+
+class FakeFilter extends Fake implements Filter {
+  @override
+  String toString() => _filter;
+}
