2027dc8027725f5ec60c0820c5e455be7190182a
Paul Nettleton <k9@k9withabone.dev>
2023-01-03 17:16:10 -0600

    Add UpdateLibraryReqParams tests

--- lib/src/models/request_parameters/update_library_req_params.dart
+++ lib/src/models/request_parameters/update_library_req_params.dart
@@ -20,6 +20,7 @@ class UpdateLibraryReqParams {
   final int? displayOrder;
   final LibraryIcon? icon;
   final MetadataProvider? provider;
+  final UpdateLibrarySettingsReqParams? settings;
 
   /// See [Update a Library](https://api.audiobookshelf.org/#update-a-library)
   const UpdateLibraryReqParams({
@@ -29,6 +30,7 @@ class UpdateLibraryReqParams {
     this.displayOrder,
     this.icon,
     this.provider,
+    this.settings,
   });
 
   Map<String, dynamic>? toJson() {
@@ -37,7 +39,10 @@ class UpdateLibraryReqParams {
       return null;
     }
     if (folders != null || newFolders != null) {
-      json['folders'] = [...?folders, ...?newFolders];
+      json['folders'] = [
+        ...?folders?.map((e) => e.toJson()),
+        ...?newFolders?.map((e) => e.toJson()),
+      ];
     }
     return json;
   }
---
+++ test/models/request_parameters/update_library_req_params_test.dart
@@ -0,0 +1,69 @@
+import 'package:audiobookshelf_api/audiobookshelf_api.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('UpdateLibraryReqParams', () {
+    const name = 'name';
+    const displayOrder = 1;
+    const icon = LibraryIcon.audiobookshelf;
+    const provider = MetadataProvider.audible;
+
+    final mockFolder = MockFolder();
+    final folders = [mockFolder];
+    final mockNewFolder = MockNewFolder();
+    final newFolders = [mockNewFolder];
+    final mockSettings = MockUpdateLibrarySettingsReqParams();
+
+    late UpdateLibraryReqParams sut;
+
+    setUp(() {
+      sut = UpdateLibraryReqParams(
+        name: name,
+        folders: folders,
+        newFolders: newFolders,
+        displayOrder: displayOrder,
+        icon: icon,
+        provider: provider,
+        settings: mockSettings,
+      );
+    });
+
+    tearDown(() {
+      reset(mockFolder);
+      reset(mockNewFolder);
+      reset(mockSettings);
+    });
+
+    group('toJson', () {
+      test('toJson', () {
+        const testMap = {'test': 'test'};
+        when(() => mockFolder.toJson()).thenReturn(testMap);
+        when(() => mockNewFolder.toJson()).thenReturn(testMap);
+        when(() => mockSettings.toJson()).thenReturn(testMap);
+        expect(sut.toJson(), {
+          'name': name,
+          'folders': [testMap, testMap],
+          'displayOrder': displayOrder,
+          'icon': icon.name,
+          'provider': provider.value,
+          'settings': testMap,
+        });
+        verify(() => mockFolder.toJson()).called(1);
+        verify(() => mockNewFolder.toJson()).called(1);
+        verify(() => mockSettings.toJson()).called(1);
+      });
+
+      test('remove defaults', () {
+        expect(const UpdateLibraryReqParams().toJson(), isNull);
+      });
+    });
+  });
+}
+
+class MockFolder extends Mock implements Folder {}
+
+class MockNewFolder extends Mock implements NewFolder {}
+
+class MockUpdateLibrarySettingsReqParams extends Mock
+    implements UpdateLibrarySettingsReqParams {}
