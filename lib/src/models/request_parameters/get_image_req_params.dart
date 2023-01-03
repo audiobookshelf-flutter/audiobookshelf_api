97acbf3bbf7d90f7ae82da3a011577722ba421c6
Paul Nettleton <k9@k9withabone.dev>
2023-01-03 01:07:20 -0600

    Add GetImageReqParams tests

--- lib/src/models/request_parameters/get_image_req_params.dart
+++ lib/src/models/request_parameters/get_image_req_params.dart
@@ -5,13 +5,16 @@ part 'generated/get_image_req_params.g.dart';
 
 @requestToJsonRemoveNull
 class GetImageReqParams {
-  static const defaults = {'width': 400};
+  static const defaults = {
+    'width': 400,
+    'raw': 0,
+  };
 
   final int width;
   final int? height;
   final String? format;
   @BoolBinaryConverter()
-  final bool? raw;
+  final bool raw;
 
   /// See [Get a Library Item's Cover](https://api.audiobookshelf.org/#get-a-library-item-39-s-cover)
   /// or [Get an Author's Image](https://api.audiobookshelf.org/#get-an-author-39-s-image)
@@ -19,7 +22,7 @@ class GetImageReqParams {
     this.width = 400,
     this.height,
     this.format,
-    this.raw,
+    this.raw = false,
   });
 
   Map<String, dynamic>? toJson() =>
