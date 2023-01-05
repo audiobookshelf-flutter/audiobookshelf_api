2f01db4054fe73a1f20f8f0480f935a8f3fb4e7b
Paul Nettleton <k9@k9withabone.dev>
2023-01-05 00:05:57 -0600

    Add BatchUpdateItemResponse tests

---
+++ test/models/responses/batch_update_item_response_test.dart
@@ -0,0 +1,30 @@
+import 'package:audiobookshelf_api/audiobookshelf_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('BatchUpdateItemResponse', () {
+    const bool success = true;
+    const int updates = 1;
+    const json = {
+      'success': success,
+      'updates': updates,
+    };
+
+    late BatchUpdateItemResponse sut;
+
+    setUp(() {
+      sut = const BatchUpdateItemResponse(
+        success: success,
+        updates: updates,
+      );
+    });
+
+    test('fromJson', () {
+      expect(sut, BatchUpdateItemResponse.fromJson(json));
+    });
+
+    test('toJson', () {
+      expect(sut.toJson(), json);
+    });
+  });
+}
