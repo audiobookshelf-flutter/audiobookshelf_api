073de123b2f3d0f4127ce3977686d8d7f7d85383
Paul Nettleton <k9@k9withabone.dev>
2023-01-13 00:27:00 -0600

    Add EventAudioFile tests

--- test/models/socket_events/audio_metadata_event_test.dart
+++ test/models/socket_events/audio_metadata_event_test.dart
@@ -3,9 +3,13 @@ import 'package:audiobookshelf_api/src/utils/json_converters.dart';
 import 'package:test/test.dart';
 
 import '../../matchers.dart';
+import 'json/audio_file_metadata_event_json.dart' as audio_file_metadata_event;
 
 void main() {
-  group('AudioMetadataEvent', () {
+  group('AudioMetadataEvent', tags: [
+    'has_dependencies',
+    'depends_on_AudioFileMetadataEvent',
+  ], () {
     const userId = 'root';
     const libraryItemId = 'li_8gch9ve09orgn4fdz8';
     const startedAtJson = 1671741903065;
@@ -28,7 +32,7 @@ void main() {
       'libraryItemId': libraryItemId,
       'startedAt': startedAtJson,
       'audioFiles': <Map<String, Object>>[],
-      'results': <Map<String, Object>>[],
+      'results': [audio_file_metadata_event.jsonFinished],
       'elapsed': elapsedJson,
       'finishedAt': finishedAtJson,
     };
@@ -48,7 +52,7 @@ void main() {
         libraryItemId: libraryItemId,
         startedAt: startedAt,
         audioFiles: [],
-        results: [],
+        results: [audio_file_metadata_event.audioFileMetadataEventFinished],
         elapsed: elapsed,
         finishedAt: finishedAt,
       );
@@ -99,6 +103,36 @@ void main() {
       test('finished', () {
         expect(sutFinished.variant, AudioMetadataEventVariant.finished);
       });
+    });
+  });
+
+  group('EventAudioFile', () {
+    const index = 1;
+    const ino = '649644248522215260';
+    const filename = 'Terry Goodkind - SOT Bk01 - Wizards First Rule 01.mp3';
+
+    const json = {
+      'index': 1,
+      'ino': ino,
+      'filename': filename,
+    };
+
+    late EventAudioFile sut;
+
+    setUp(() {
+      sut = const EventAudioFile(
+        index: index,
+        ino: ino,
+        filename: filename,
+      );
+    });
+
+    test('fromJson', () {
+      expect(sut, EventAudioFile.fromJson(json));
+    });
+
+    test('toJson', () {
+      expect(sut.toJson(), json);
     });
   });
 }
