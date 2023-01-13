80b2065a6086610124b34755fa1a09ccb2c51d1a
Paul Nettleton <k9@k9withabone.dev>
2023-01-13 00:48:29 -0600

    Add LibraryScan tests

---
+++ test/models/socket_events/library_scan_test.dart
@@ -0,0 +1,57 @@
+import 'package:audiobookshelf_api/audiobookshelf_api.dart';
+import 'package:test/test.dart';
+
+import '../../matchers.dart';
+
+void main() {
+  const added = 0;
+  const updated = 0;
+  const missing = 0;
+
+  const libraryScanResultsJson = {
+    'added': 0,
+    'updated': 0,
+    'missing': 0,
+  };
+
+  const libraryScanResults = LibraryScanResults(
+    added: added,
+    updated: updated,
+    missing: missing,
+  );
+
+  group('LibraryScan', tags: [
+    'has_dependencies',
+    'depends_on_LibraryScanResults',
+  ], () {
+    const libraryId = 'lib_yq748byukae93rulli';
+    const type = 'scan';
+    const name = 'Audiobooks';
+
+    const json = {
+      'id': libraryId,
+      'type': type,
+      'name': name,
+      'results': libraryScanResultsJson,
+    };
+
+    late LibraryScan sut;
+
+    setUp(() {
+      sut = const LibraryScan(
+        libraryId: libraryId,
+        type: type,
+        name: name,
+        results: libraryScanResults,
+      );
+    });
+
+    test('fromJson', () {
+      expect(sut, LibraryScan.fromJson(json));
+    });
+
+    test('toJson', () {
+      expect(sut.toJson(), deepMapContains(json));
+    });
+  });
+}
